c392b4d10f5f022ad8b256d3466de7a3ddb979c7
uglyprincess89 <uglyprincess89@github.com>
2026-05-07 16:31:06 +0200

    ostry email noreply@contact.xt-invest.cz

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: NextRequest) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      from: 'onboarding@resend.dev',
+      from: 'XT-Invest web <noreply@xt-invest.cz>',
       to: ['info@xt-invest.cz'],
       reply_to: email,
       subject: `Poptávka: ${zajem} | ${jmeno}`,
